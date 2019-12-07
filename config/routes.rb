ae1ff2c4d7c0ab766631150a3609be2a243c13ce
Your Name <minamoyori19@outlook.jp>
2019-12-07 12:06:34 +0900

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   devise_for :admins
-  devise_for :customers
+
+  scope module: 'customers' do
+  devise_for :customers, controllers: {
+    sessions: 'customers/sessions',
+    registrations: 'customers/registrations',
+    passwords: 'customers/passwords'
+  }
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
