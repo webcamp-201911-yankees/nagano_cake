58e6b34a42565b4572a166eb1a3331984fc180bd
yusuke-hub <yk1210140408@gmail.com>
2019-12-07 05:24:36 +0000

    add namespace config

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,20 @@ Rails.application.routes.draw do
   devise_for :admins
   devise_for :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :manager do
+      devise_for :admin_customers, controllers: {
+      sessions: 'client/admin_customers/sessions',
+      registrations: 'client/admin_customers/registrations',
+      passwords: 'client/admin_customers/passwords'
+    }
+  end
+
+    scope module: 'site' do
+    devise_for :customers, controllers: {
+      sessions: 'site/customers/sessions',
+      registrations: 'site/customers/registrations',
+      passwords: 'site/customers/passwords'
+    }
+  end
+
 end
