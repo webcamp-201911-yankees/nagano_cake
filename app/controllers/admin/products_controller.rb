2e4bc4fd075fdc09d6ae6e8da71be87668847b9d
haruyamataiki <haruyamataiki619@gmali.com>
2019-12-08 04:44:17 +0000

    a_product

---
+++ app/controllers/admin/products_controller.rb
@@ -0,0 +1,28 @@
+class Admin::ProductsController < ApplicationController
+  def index
+  	@products = Product.all
+  end
+
+  def new
+  	@product = Product.new
+  end
+
+  def create
+  	@product = Product.new(product_params)
+  	@product.save
+  	redirect_to  products_path
+  end
+
+  def edit
+  end
+  
+
+  def show
+  end
+
+  private
+  def product_params
+  	params.require(:product).permit(:product_image,:caption,:product_name,:category_id,:tax_excluded,:sale_status)
+  end
+end
+
