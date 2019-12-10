ee912ed0e02025aa931c9ab3833b04fbbf038be1
Your Name <minamoyori19@outlook.jp>
2019-12-10 11:21:59 +0900

    first

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -4,11 +4,21 @@ class Admin::CustomersController < ApplicationController
 	end
 
 	def show
+		@customer = Customer.find(params[:id])
+	end
 
+	def edit
+	  	@customer = Customer.find(params[:id])
+	end
+
+	def update
+	  	customer = Customer.find(params[:id])
+	  	customer.update(customer_params)
+	  	redirect_to admin_customer_path(customer.id)
 	end
 
 	private
 	def customer_params
-	  	params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :zipcode, :address, :phone_number, :email, :password, :account_status)
+	  	params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :zipcode, :address, :phone_number, :email, :encrypted_password, :account_status)
 	end
 end
--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -12,13 +12,13 @@ class CustomersController < ApplicationController
   end
 
   def update
-  	@customer = Customer.find(params[:id])
-  	@customer.update(customer_params)
-  	redirect_to customer_path(@customer.id)
+  	customer = Customer.find(params[:id])
+  	customer.update(customer_params)
+  	redirect_to customer_path(customer.id)
   end
 
   private
   def customer_params
-  	params.require(:customers).permit(:email,:encrypted_password, :last_name, :first_name, :last_name_kana, :first_name_kana, :zipcode, :address, :phone_number, :account_status)
+  	params.require(:customer).permit(:email,:encrypted_password, :last_name, :first_name, :last_name_kana, :first_name_kana, :zipcode, :address, :phone_number, :account_status)
   end
 end
