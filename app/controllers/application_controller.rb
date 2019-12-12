9ec8c49d260134673d7cf8be00d6a7bbcf1a1381
Your Name <minamoyori19@outlook.jp>
2019-12-12 17:33:04 +0900

    first

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,6 +27,13 @@ class ApplicationController < ActionController::Base
 		# flash[:signedin] ="Signed in successfully."
 	end
 
+	before_action :set_search
+
+	def set_search
+	    @search = Product.ransack(params[:q])
+	    @results = @search.result
+	end
+
 
 protected
 
