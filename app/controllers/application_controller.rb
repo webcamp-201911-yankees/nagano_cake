7f0fbeb97dcf3cbf0805830efa36d513e6618668
kashio-tech <gumgum.yk.29@gmail.com>
2019-12-23 13:37:11 +0900

    コメント付き

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,8 +30,13 @@ class ApplicationController < ActionController::Base
 
 	def set_search
 		@categories = Category.all
-	    @search = Product.ransack(params[:q])
-	    @results = @search.result.page(params[:page]).per(12)
+		# search1はカテゴリーの有効・無効や商品の販売中・停止を反映した上topページでランサックを用いたワード検索するための記述
+		@search1 = Product.ransack(params[:q])
+		
+		# search2はカテゴリーの有効・無効や商品の販売中・停止を反映した上でカテゴリー検索するための記述
+	    @search2 = Product.joins(:category).where('categories.status = ? AND sale_status = ?', 1, 0).ransack(params[:q])
+	    @results1 = @search1.result.page(params[:page]).per(12)
+	    @results2 = @search2.result.page(params[:page]).per(12)
 	end
 
 
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,8 +1,9 @@
 class Category < ApplicationRecord
 	has_many :products
 	validates :name, presence: true, uniqueness: true
-	enum status: { 無効: 0, 有効: 1}
+	enum status: { 無効: 0, 有効: 1} #enumの記述方式
 
+# toggle_statusメソッドを定義(admin_categoryのcontrollerで使用)
 	def toggle_status
 		if status == "有効"
 			status = 0
